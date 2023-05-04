d517717e312d0c674b16b4dfef7c408d89c8a88a
rafaelspa <rsdepaula67@gmail.com>
2023-05-04 19:32:20 -0300

    LP1: projeto maven nova estrutura

---
+++ LP1A3_Projeto1-main/LP1A3_Projeto1/src/main/java/org/lp1_grupo2/interfaces/GerenciamentoDeUsuarios.java
@@ -0,0 +1,12 @@
+package org.lp1_grupo2.interfaces;
+
+public interface GerenciamentoDeUsuarios{
+
+    public void cadastrarUsuario();
+
+    public void atualizarUsuario();
+
+    public void removerUsuario();
+
+    public void buscarUsuario();
+}

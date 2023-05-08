bce56d9575f0f621d719cb8a689445f048ce6f14
rafaelspa <rsdepaula67@gmail.com>
2023-05-08 05:02:27 -0300

    Projeto 1 - Spring: interfaces

--- Projeto1-spring/biblioteca_api/biblioteca_api/src/main/java/com/grupo2/biblioteca_api/interfaces/GerenciamentoDeLivros.java
+++ Projeto1-spring/biblioteca_api/biblioteca_api/src/main/java/com/grupo2/biblioteca_api/interfaces/GerenciamentoDeLivros.java
@@ -1,4 +1,10 @@
 package com.grupo2.biblioteca_api.interfaces;
 
+import com.grupo2.biblioteca_api.livro.Livro;
+
 public interface GerenciamentoDeLivros {
+    void cadastrarLivro(Livro livro);
+    void atualizarLivro(Livro livro);
+    void removerLivro(Livro livro);
+    void buscaLivro(Livro livro);
 }
--- Projeto1-spring/biblioteca_api/biblioteca_api/src/main/java/com/grupo2/biblioteca_api/interfaces/GerenciamentoDeUsuarios.java
+++ Projeto1-spring/biblioteca_api/biblioteca_api/src/main/java/com/grupo2/biblioteca_api/interfaces/GerenciamentoDeUsuarios.java
@@ -1,4 +1,10 @@
 package com.grupo2.biblioteca_api.interfaces;
 
+import com.grupo2.biblioteca_api.usuario.Usuario;
+
 public interface GerenciamentoDeUsuarios {
+    void cadastrarUsuario(Usuario usuario);
+    void atualizarUsuario(Usuario usuario);
+    void removerUsuario(Usuario usuario);
+    void buscaUsuario(Usuario usuario);
 }
